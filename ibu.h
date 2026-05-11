ec74a7352c8774c489b886f199c18e3155c39c92
hudangrh <hudangrhkuliah@gmail.com>
2026-05-11 15:55:37 +0700

    Class Ibu

---
+++ ibu.h
@@ -0,0 +1,17 @@
+#ifndef IBU_H
+#define IBU_H
+#include <vector>
+
+class ibu 
+{
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama "\"tidak ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" todak ada\n";
+    }
+}
